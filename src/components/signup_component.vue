949847a2007a51ba5aba1a09c182f6c55a6db446
peacemaker19881 <jeandelapaix2013@gmail.com>
2024-04-23 18:17:27 +0200

    my logo and file change

--- src/components/signup_component.vue
+++ src/components/signup_component.vue
@@ -1,5 +1,12 @@
 <template>
+    <img src="../assets/download.jpg"/>
 <h1>Welcome on sign up form</h1>
+<div class="register">
+    <input type="text" placeholder="Enter your name"/><br><br>
+    <input type="text" placeholder="Enter your name"/><br><br>
+    <input type="password" placeholder="Enter your password"/><br><br>
+    <button>Sign up</button>
+</div>
 </template>
 
 <script>
@@ -9,5 +16,14 @@ export default{
 }
 </script>
 <style>
-
+.register input{
+width: 300px;
+height: 40px;
+padding-left: 20px;
+display: block;
+margin-bottom: 30px;
+margin-right: auto;
+margin-left: auto;
+border: 1px solid skyblue;
+}
 </style>
