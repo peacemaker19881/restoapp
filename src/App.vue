c56654927edea7a5da7b6cd1a03ddbfdca3bb361
peacemaker19881 <jeandelapaix2013@gmail.com>
2024-04-23 17:17:57 +0200

    my first commit after creating my application on restaurent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <signup_component/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import signup_component from './components/signup_component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    signup_component,
   }
 }
 </script>
---
+++ src/components/signup_component.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Welcome on sign up form</h1>
+</template>
+
+<script>
+export default{
+    name:'signup_component',
+    components:{}
+}
+</script>
+<style>
+
+</style>
